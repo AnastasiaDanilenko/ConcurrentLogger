bc996a25b7c8016409a8ee5ee069d00b207f12fa
AnastasiaDanilenko <451005asya@gmail.com>
2016-11-03 10:22:32 +0300

    Threads added

--- ConcurrentLogger/Logger.cs
+++ ConcurrentLogger/Logger.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Text;
 using System.Threading.Tasks;
 using static ConcurrentLogger.Program;
@@ -10,17 +11,35 @@ namespace ConcurrentLogger
     public class Logger:ILogger
     {
         int bufferLimit;
+        int actualLoggers;
+        int threads;
         ILoggerTarget[] listTargets = null;
+        List<string> logInfo = new List<string>();
         public Logger(int bufferLimit, ILoggerTarget[] targets)
         {
             this.bufferLimit = bufferLimit;
             listTargets = targets;
+            actualLoggers = 0;
+            threads = 0;
         }
 
         public void Log(LogLevel level, string message)
         {
+            actualLoggers++;
+            if (actualLoggers == bufferLimit)
+            {
+                ThreadPool.QueueUserWorkItem(FlushThreads, logInfo);
+                threads++;
+                logInfo.Clear();
+            }
+            logInfo.Add(level.ToString() + message);
+        }
+
+        public void FlushThreads(object logs)
+        {
 
         }
+
     }
 
     public interface ILogger
